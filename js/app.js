0b3af131c40e1168e23938cb376865c1e62b98b2
EbbaSchmid <justebba@gmail.com>
2022-08-30 21:30:56 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log ("hello")
+//create cached element references for each element (input, button & ul)
+
+
+// CER for input element.........................
+
+const input = document.querySelector("input")
+
+// CER for button element.........................
+
+const button = document.querySelector("submit-button")
+
+// CER for ul element.........................
+const ul = document.querySelector("todo-list")
+
+
+// Step 6...............................
+
+button.addEventListener('click', function(evt) {
+    const li = document.createElement('li')
+    console.log(li)
+});
+
+li.textContent = inp.value 
+document.querySelector('ul').appendChild(li)
+
