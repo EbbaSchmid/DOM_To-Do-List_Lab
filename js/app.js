9e9f90b0584b250ff477fc60bf2a06a413146c1d
EbbaSchmid <justebba@gmail.com>
2022-08-31 09:36:18 -0500

    update

--- js/app.js
+++ js/app.js
@@ -3,23 +3,24 @@
 
 // CER for input element.........................
 
-const input = document.querySelector("input")
+const input = document.getElementById("input")
 
 // CER for button element.........................
 
-const button = document.querySelector("submit-button")
+const button = document.getElementById("submit-button")
 
 // CER for ul element.........................
-const ul = document.querySelector("todo-list")
+const ul = document.getElementById("todo-list")
 
 
 // Step 6...............................
 
 button.addEventListener('click', function(evt) {
     const li = document.createElement('li')
+    li.textContent = input.value 
     console.log(li)
 });
 
-li.textContent = inp.value 
+
 document.querySelector('ul').appendChild(li)
 
