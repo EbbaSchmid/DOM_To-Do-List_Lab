d58e3e2ff429805d4d7e9240cdd7831cdb24eb82
EbbaSchmid <justebba@gmail.com>
2022-08-31 15:04:51 -0500

    update

--- js/app.js
+++ js/app.js
@@ -1,34 +1,26 @@
 //create cached element references for each element (input, button & ul)
 
-
 // CER for input element.........................
-
 const input = document.getElementById("input")
 
 // CER for button element.........................
-
 const button = document.getElementById("submit-button")
 
 // CER for ul element.........................
 const ul = document.getElementById("todo-list")
 
-
-// Step 6...............................
-
-button.addEventListener('click', function(evt) {
-    const li = document.createElement('li')
-    li.textContent = input.value
-})
-
 // CER for reset button.........................
 const reset = document.getElementById('reset-button')
 
-document.querySelector('ul').addEventListener('click', Click)
-
-
-reset.addEventListener('click', function(evt) {
-    ul.innerHTML = ''
-    input.value = ''
+// Step 6...............................
+button.addEventListener('click', function(evt) {
+    const li = document.createElement('li')
+    li.textContent = input.value
+    document.querySelector('ul').appendChild(li)
+    input.value = ""
 })
 
-document.querySelector('ul').addEventListener('click', handleClick)
+
+// CER for click.........................
+// const ul = document.createElement('ul').addEventListener('click', Click)
+
